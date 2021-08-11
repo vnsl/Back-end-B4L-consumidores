223d5ff39e8d882b1f7c352ec7d9973401158948
Luiz Felipe <luizfelipe90@hotmail.com>
2021-08-11 17:58:33 -0300

    Criado rota e função para listar restaurantes

--- src/rotas.js
+++ src/rotas.js
@@ -2,6 +2,7 @@ const express = require('express');
 const login = require('./controladores/login');
 const consumidores = require('./controladores/consumidores');
 const imagem = require('./controladores/imagem');
+const restaurantes = require('./controladores/restaurantes');
 const autenticacao = require('./filtros/autenticacao');
 
 const rotas = express();
@@ -14,6 +15,9 @@ rotas.post('/consumidores', consumidores.cadastrarConsumidor);
 
 // verificação de autenticação
 rotas.use(autenticacao);
+
+// listar restaurantes
+rotas.get('/restaurantes', restaurantes.listarRestaurantes)
 
 // envio da imagem
 rotas.post('/upload', imagem.enviarImagem);
