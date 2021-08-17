31fc9e2a9b9c8692981d0a52400b3dd95e5e3371
jeanrvos <velasco.jean@gmail.com>
2021-08-17 07:44:36 -0300

    update do schema

--- schema.sql
+++ schema.sql
@@ -5,3 +5,21 @@ create table if not exists consumidor (
 	telefone text not null,
   	senha text not null
 );
+
+create table if not exists pedido (
+	id serial primary key,
+	consumidor_id int not null,
+	valor_pedido int not null,
+  	foreign key (consumidor_id) references consumidor (id),
+);
+
+create table if not exists detalhepedido (
+	id serial primary key,
+	pedido_id int not null,
+	produto_id int not null,
+	quantidade_produto int not null,
+	valor_total int not null,
+  	foreign key (pedido_id) references pedido (id),
+  	foreign key (produto_id) references produto (id),
+);
+
