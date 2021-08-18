ac3a366c797089ce860cd10a098585b29fe6fe45
Luiz Felipe <luizfelipe90@hotmail.com>
2021-08-18 13:11:19 -0300

    Atualizado função para cadastrar endereço

--- src/controladores/endereco.js
+++ src/controladores/endereco.js
@@ -5,14 +5,14 @@ const cadastrarEndereco = async (req, res) => {
     const { endereco } = req.body;
 
     try {
-        const { dadosConsumidor } = consumidor;        
-        const usuarioEncontrado = await knex('consumidor').where('id', consumidor.id);
+
+        const usuarioEncontrado = await knex('consumidor').where('id', consumidor.dadosConsumidor.id);
 
         if(!usuarioEncontrado[0]){
             res.status(404).json('Usuário não encontrado');
         }
 
-        const cadastrarEndereco = await knex('endereco').insert(endereco).returning('*');
+        const cadastrarEndereco = await knex('endereco').insert({'consumidor_id': consumidor.dadosConsumidor.id, 'endereco': endereco}).returning('*');
 
         if (cadastrarEndereco.length === 0) {
             return res.status(400).json("O endereço não foi cadastrado.");
