4b635afed0bcc54b50ac0e5e09a7c0e87b86194e
Vinicius <viniciusnattan14@gmail.com>
2021-08-18 20:19:00 -0300

    erro

--- src/controladores/produtos.js
+++ src/controladores/produtos.js
@@ -7,19 +7,33 @@ const listarProdutos = async (req, res) => {
     try {
         const listaDeProdutos = await knex('produto').where('restaurante_id', id);
 
-         if(!listaDeProdutos[0]){
-            return res.status(404).json('Não foi encontrado nenhum produto neste restaurante');
-        };
+        //  if(!listaDeProdutos[0]){
+        //     return res.status(404).json('Não foi encontrado nenhum produto neste restaurante');
+        // };
 
         const cardapio = await knex('produto').where('ativo', true).andWhere('restaurante_id', id);
 
-        if(!cardapio[0]){
-            return res.status(404).json('Restaurante não possui produtos ativos');
-        }
+        // if(!cardapio[0]){
+        //     return res.status(404).json('Restaurante não possui produtos ativos');
+        // }
 
         const dadosRestaurante = await knex('restaurante').where('id', id);
 
-        const restaurante = { restaurante: dadosRestaurante[0], cardapio };
+        // const restaurante = { restaurante: dadosRestaurante[0], cardapio };
+
+        const restaurante = {
+            id: dadosRestaurante[0].id,
+            nome: dadosRestaurante[0].nome,
+            categoria_id: dadosRestaurante[0].categoria_id,
+            url_categoria: 
+            taxa_entrega: dadosRestaurante[0].taxa_entrega,
+            tempo_entrega_minutos: dadosRestaurante[0].tempo_entrega_minutos,
+            valor_minimo_pedido: dadosRestaurante[0].valor_minimo_pedido,
+            imagem: dadosRestaurante[0].imagem,
+            descricao: dadosRestaurante[0].descricao,
+            cardapio: cardapio
+        }
+
 
         return res.status(200).json(restaurante);
 
